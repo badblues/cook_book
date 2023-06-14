18f679aa3d9f59c73a6aa211c237b20b51780ab9
badbadblues _ <borovikxv.a@gmail.com>
2023-06-14 13:52:16 +0700

    make Recipie model

---
+++ backend/Domain/Recipie.cs
@@ -0,0 +1,9 @@
+﻿namespace CookBook.Domain;
+
+public class Recipie
+{
+  string Name = "";
+  string Base64Icon = "";
+  string Description = "";
+  (string, string)[] StepsImageAndDescription = {};
+}
