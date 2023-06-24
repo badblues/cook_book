7863103f9a8da4afdac54c21b33d00ad778be71c
badblues <borovikxv.a@gmail.com>
2023-06-24 13:22:08 +0700

    add WebApi configuration

--- backend/WebApi/Program.cs
+++ backend/WebApi/Program.cs
@@ -1,11 +1,22 @@
+using CookBook.Domain;
+using CookBook.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json", optional: false);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//TODO must be a better way of doing this
+string? storagePath =
+    builder.Configuration.GetSection("Storage:StoragePath").Value
+    ?? throw new NullReferenceException();
+builder.Services.AddScoped<IRepository<Recipe>>(provider => 
+{
+    return new StorageRepository(storagePath);
+});
 
 var app = builder.Build();
 
@@ -15,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseHttpsRedirection();
 
